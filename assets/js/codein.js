5b7e01bb9874931230064ec9998f5488469dcaf6
김수민 <sumin@gimsumin-ui-noteubug.local>
2024-10-21 21:38:54 +0900

    add gen

---
+++ assets/js/codein.js
@@ -0,0 +1,12 @@
+function Generate_image() {
+    // $(".game_image").attr("src", "./img/gameui.gif");
+    // $(".gen_btn").attr("disabled", true);
+    // // 서버 과부하 방지를 위해 여기서부터  gif인지, 크기는 어떤지등 확인하고 alert 해도 좋음
+    //
+    // setTimeout(() => console.log("after"), 1000); // 호출한다음 값을 받으면
+    //
+    // // $(".game_image").attr("src", "./img/gameimg.png");
+    // // $(".gen_btn").attr("disabled", false);
+
+    alert("coming soon")
+}
