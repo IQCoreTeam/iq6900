603cd8cdb138b5111aef4be4934d3c2fd541110a
sumin <dwckey5356@gmail.com>
2024-12-31 15:57:10 +0900

    dd

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -3,7 +3,7 @@ $(function(){
     $(".home_button").click(function(){
         if($("#main_page").length == 0)
         {
-            location.href = "https://iq6900.com/";
+            $.mainPage.init();
         }
 
     });
