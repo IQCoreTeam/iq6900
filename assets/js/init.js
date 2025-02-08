9297e506d4a0f1a3286f70e0fb44ae5f8d831ad8
sumin <dwckey5356@gmail.com>
2025-02-09 04:25:51 +0900

    dd

--- assets/js/init.js
+++ assets/js/init.js
@@ -5,19 +5,7 @@ function getQueryParams() {
         txid: urlParams.get('txid')
     };
 }
-function toggleClassForMenu(menu1,menu2, oldClass, newClass) {
-    if ($(menu1).hasClass(oldClass)) {
-        $(menu1).removeClass(oldClass)
-        $(menu1).addClass(newClass);
 
-        $(menu2).removeClass(newClass)
-        $(menu2).addClass(oldClass);
-    }
-}
-function goto_viewer(){
-    toggleClassForMenu('#go_viewer','#go_code_in',"img_button","img_button_green");
-    $.checkSection.init();
-}
 
 $(document).ready(function() {
     const { txid } = getQueryParams();
