682d0ff4a730b34b6a884c7f08ea47e4e6cae625
sumin <dwckey5356@gmail.com>
2025-02-09 04:29:49 +0900

    dd

--- assets/js/check_contract.js
+++ assets/js/check_contract.js
@@ -549,27 +549,3 @@ async function seeTransaction(txid) {
 
 
 }
-$(document).ready(async function () {
-    const {txid} = getQueryParams();
-    if (txid) {
-        await goto_viewer();
-        const urlWithoutParams = window.location.protocol + "//" + window.location.host + window.location.pathname;
-        window.history.replaceState({}, '', urlWithoutParams);
-
-        await seeTransaction(txid);
-    }
-});
-function toggleClassForMenu(menu1, menu2, oldClass, newClass) {
-    if ($(menu1).hasClass(oldClass)) {
-        $(menu1).removeClass(oldClass)
-        $(menu1).addClass(newClass);
-
-        $(menu2).removeClass(newClass)
-        $(menu2).addClass(oldClass);
-    }
-}
-
-function goto_viewer() {
-    toggleClassForMenu('#go_viewer', '#go_code_in', "img_button", "img_button_green");
-    $.checkSection.init();
-}
