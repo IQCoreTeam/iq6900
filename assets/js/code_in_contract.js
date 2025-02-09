8929ca03f18cc43f999b90482ae08046d64a8511
sumin <dwckey5356@gmail.com>
2025-02-09 12:06:06 +0900

    dd

--- assets/js/code_in_contract.js
+++ assets/js/code_in_contract.js
@@ -168,7 +168,7 @@ async function createInitTransactionOnServer(userKeyString) {
             try {
                 const responseData = await response.json();
                 const data = responseData.transaction;
-                const transaction = await _translate_transaction(data);
+                const transaction = await _translate_transaction(data.transaction);
                 return transaction;
             } catch (error) {
                 console.error('Error creating transaction:', error);
