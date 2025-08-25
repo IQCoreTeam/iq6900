aa6659606c20d65baa5a5348a5580ce95ca8959b
sumin <dwckey5356@gmail.com>
2025-08-25 19:14:27 +0900

    changed to recent data

--- assets/js/check_contract.js
+++ assets/js/check_contract.js
@@ -812,7 +812,7 @@ async function bringDataHandler(targetAddress, menu) {
 
 async function publicSearch() {
     try {
-        let IQContractKeyString = "GbgepibVcKMbLW6QaFrhUGG34WDvJ2SKvznL2HUuquZh";
+        let IQContractKeyString = "EWNSTD8tikwqHMcRNuuNbZrnYJUiJdKq9UXLXSEU4wZ1";
         await bringDataHandler(IQContractKeyString, "SolanaInternet");
     } catch (err) {
         console.error(err);
