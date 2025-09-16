4b6394549b0aea1acf6495c24ac71d2446ae2d78
sumin <dwckey5356@gmail.com>
2025-09-16 16:24:09 +0900

    fetch

--- assets/js/decoder/mp3_decoder.js
+++ assets/js/decoder/mp3_decoder.js
@@ -258,10 +258,10 @@ async function startFetcher(txList) {
             frameQueue[i] = fallbackFrame();
         }
     }
-    // completedFetchers++;
-    // if (completedFetchers === MAX_CONCURRENT_FETCHERS) {
-    //     isDone = true;
-    // }
+    completedFetchers++;
+    if (completedFetchers === MAX_CONCURRENT_FETCHERS) {
+        isDone = true;
+    }
 }
 
 function fallbackFrame() {
@@ -283,10 +283,10 @@ async function startVideo() {
     const txList = content.trim().split("\n").map(line => line.trim());
 
     // Bring frames from blockchain,
-    // for (let i = 0; i < MAX_CONCURRENT_FETCHERS; i++) {
-    //     startFetcher(txList);
-    // }
-    startFetcher(txList);
+    for (let i = 0; i < MAX_CONCURRENT_FETCHERS; i++) {
+        startFetcher(txList);
+    }
+    // startFetcher(txList);
 
     // start render
     await new Promise(res => setTimeout(res, 200));
