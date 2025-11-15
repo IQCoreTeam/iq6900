067670dc005499af8ba5b493cd119832221be8c3
sumin <dwckey5356@gmail.com>
2025-11-15 13:16:44 +0900

    maybe old ver

--- assets/js/code_in_contract.js
+++ assets/js/code_in_contract.js
@@ -1,4 +1,4 @@
-const host = "https://iq-testbackend-381334931214.asia-northeast3.run.app";
+const host = "https://iq6900-backend-381334931214.asia-northeast3.run.app";
 const network = "https://mainnet.helius-rpc.com/?api-key=c3096a77-0ebb-4144-82cf-904f7d134780";
 // import { BrowserSDK, AddressType, NetworkId } from "@phantom/browser-sdk";
 // const ptSdk = new phantomwalletSdk.BrowserSDK({
