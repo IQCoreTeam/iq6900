039214af9de0e8d926cea27e2fbfe70753c8fdf4
sumin <dwckey5356@gmail.com>
2026-08-01 20:22:48 +0900

    Replace AgentNet card buttons with thread and download links

--- assets/js/sections/pages/main_page.js
+++ assets/js/sections/pages/main_page.js
@@ -6,7 +6,7 @@
     function mainPage() {
         let $mainPageElement;
         let $wWidth = screen.width;
-        const $mainPageTemplateUrl = "./html/sections/main_page.html?ver=20260801e";
+        const $mainPageTemplateUrl = "./html/sections/main_page.html?ver=20260801f";
         function init() {
             loadMainPageTemplate();
         }
